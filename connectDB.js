4599185fb62cd120fc2c28f900d8ec5fa16460ac
1neye <illya.landar@gmail.com>
2021-12-30 06:06:41 +0200

    's'

--- connectDB.js
+++ connectDB.js
@@ -1,3 +1,3 @@
 const mongoose = require('mongoose');
 
-module.exports = mongoose.createConnection('mongodb+srv://Ineye:oiacANYXfviOYm5k@cluster0.luiue.mongodb.net/Train?retryWrites=true&w=majority');
+module.exports = mongoose.createConnection('mongodb+srv://Ineye:<pass>@cluster0.luiue.mongodb.net/Train?retryWrites=true&w=majority');
